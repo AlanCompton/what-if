9193ca3b998abd366c8a85289ea02e98a46155bd
AlanCompton <alan.compton@codeimmersives.com>
2022-09-17 08:10:21 -0500

    Uploaded What-If projects.

---
+++ fizzbuzz.js
@@ -0,0 +1,13 @@
+const prompt = require(`prompt-sync`) ({signt: true});
+
+let num = Number(prompt(`Enter a number: `));
+
+console.log(num);
+
+if(num % 15 === 0){
+    console.log(`fizzbuzz`);
+}else if(num % 3 === 0){
+    console.log("fizz");
+}else if(num % 5 === 0){
+    console.log(`buzz`);
+}
